8ecb432f90da023add89392bc0451143eec74cca
Tanvir Alam <ta@casalogic.dk>
2016-12-24 23:48:30 +0100

    2.4 - Faking the Payment Gateway

--- tests/features/PurchaseTicketsTest.php
+++ tests/features/PurchaseTicketsTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentGateway;
 use App\Concert;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -8,26 +10,32 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class PurchaseTicketsTests extends TestCase
 {
-    /*use DatabaseMigrations;*/
+    use DatabaseMigrations;
 
     /** @test  */
     public function customerCanPurchaseConcertTickets()
     {
+        $paymentGateway = new FakePaymentGateway;
+
+        $this->app->instance(PaymentGateway::class, $paymentGateway);
+
         //Arrange
         //Create a concert
-        $concert = factory(Concert::class)->create(['tickets_price' => 3250]);
+        $concert = factory(Concert::class)->create(['ticket_price' => 3250]);
 
         //act
 
         //Purchase concert tickets
         $this->json('POST', "/concerts/{$concert->id}/orders", [
            'email' => 'jhon@example.com',
-            'tickets_quantity' => 3,
+            'ticket_quantity' => 3,
             'payment_token' => $paymentGateway->getValidTestToken(),
         ]);
 
 
         //Assert
+        $this->assertResponseStatus(201);
+
         //make sure the customer was changed the correct amount
         $this->assertEquals(9750, $paymentGateway->totalCharges());
 
---
+++ tests/unit/Billing/FakePaymentGatewayTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Billing\FakePaymentGateway;
+use App\Concert;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class FakePaymentGatewayTest extends TestCase
+{
+    //use DatabaseMigrations;
+
+    /** @test  */
+    public function changeWithAValidPaymentTokenAreSuccessful()
+    {
+        $paymentGateway = new FakePaymentGateway;
+
+        $paymentGateway->charge(2500, $paymentGateway->getValidTestToken());
+
+        $this->assertEquals(2500, $paymentGateway->totalCharges());
+    }
+
+    /** @test  */
+/*    public function userCannotViewUnpublishedConcertListing()
+    {
+
+    }*/
+
+}
