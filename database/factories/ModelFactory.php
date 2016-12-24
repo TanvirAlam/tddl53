030fe4b6c105200d86ebcf8c83bb0055582acea2
Tanvir Alam <ta@casalogic.dk>
2016-12-24 18:22:25 +0100

    Refractor method by calling fectory 1.4

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,15 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
+use App\Concert;
+use Carbon\Carbon;
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
@@ -19,5 +11,20 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Concert::class, function (Faker\Generator $faker) {
+    return [
+        'title' => 'Example and',
+        'subtitle' => 'With love Fake Opener',
+        'date' => Carbon::parse('+2 weeks'),
+        'ticket_price' => 2000,
+        'venue' => 'The Helsingor Kommune 99',
+        'venue_address' => 'Nordvej 19, 1tv',
+        'city' => 'FakeHeslingor',
+        'state' => 'H',
+        'zip' => '4000',
+        'addition_information' => 'Call-92165545, XXX',
     ];
 });
