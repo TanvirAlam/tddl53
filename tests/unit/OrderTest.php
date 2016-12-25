1fe47e3b12f92d1f3c2aa45c241350bc9d891b69
Tanvir Alam <ta@casalogic.dk>
2016-12-25 20:29:33 +0100

    5.5 - Precomputing the Order Amount

--- tests/unit/OrderTest.php
+++ tests/unit/OrderTest.php
@@ -14,13 +14,13 @@ class OrderTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    public function creatingAnOrderFromTicketsAndEmail()
+    public function creatingAnOrderFromTicketsAndEmailAndAmount()
     {
-        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(5);
+        $concert = factory(Concert::class)->create()->addTickets(5);
 
         $this->assertEquals(5, $concert->ticketsRemaining());
 
-        $order = Order::forTickets($concert->findTickets(3), 'john@example.com');
+        $order = Order::forTickets($concert->findTickets(3), 'john@example.com', 3600);
 
         $this->assertEquals('john@example.com', $order->email);
         $this->assertEquals(3, $order->ticketQuantity());
