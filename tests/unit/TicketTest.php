6fe0011c73b457932ae6590c20c0db280777d397
Tanvir Alam <ta@casalogic.dk>
2016-12-25 13:45:42 +0100

    3.6 - Refactoring and Redundant Test Coverage

---
+++ tests/unit/TicketTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Concert;
+use App\Order;
+use App\Exceptions\NotEnoughTicketsException;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+
+class TicketTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function aTicketCanBeReleased()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(1);
+        $order = $concert->orderTickets('jane@example.com', 1);
+        $ticket = $order->tickets()->first();
+        $this->assertEquals($order->id, $ticket->order_id);
+        $ticket->release();
+        $this->assertNull($ticket->fresh()->order_id);
+
+    }
+}
